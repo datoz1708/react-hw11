62b2e859329ea712a7f07f1e3ca116540b25c1a0
datoz1708 <Dato.zarandia.1@gmail.com>
2024-10-02 14:06:09 +0400

    ok

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import HelloWorld from "./helloworld";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <HelloWorld/>
+      </div>
+    )
+  }
+}
+
+export default App;

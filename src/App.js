1dd0a8bad1f3736ebe850cd56ade5a66c53413b4
datoz1708 <Dato.zarandia.1@gmail.com>
2024-10-13 16:13:39 +0400

    ok

--- src/App.js
+++ src/App.js
@@ -1,21 +1,17 @@
 import React from 'react';
 
+import image1 from './images/Apple-iPhone-16-Pro-hero-240909_inline.jpg.large_2x.jpg'
+import image2 from './images/Samsung-Galaxy-S24-Ultra-Titanium-Grey.jpg'
+import image3 from './images/Pixel-9-Pro.jpg'
+
 function App() {
-  const num1 = 5;
-  const num2 = 10;
-  let message;
-
-  if (num1 > num2) {
-    message = num1;
-  } else {
-    message = num2;
-  }
-
-  return (
-    <div className="App">
-      <h1>{message}</h1>
-    </div>
-  );
+    return (
+        <div className="App">
+            <img src={image1} width={300} height={300} />
+            <img src={image2} width={300} height={300} />
+            <img src={image3} width={300} height={300} />
+        </div>
+    );
 }
 
 export default App;
